a639c49f9e062372a7c9af5677229a45eb17f443
Harry Whitehorn <harrywhitehorn@outlook.com>
2024-04-22 02:48:13 +0100

    cert verif

--- app/lobbies.py
+++ app/lobbies.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from threading import Thread
 
 class Lobby:
@@ -7,3 +8,174 @@ class Lobby:
     
     def __init__(self, port):
         pass
+=======
+from threading import Thread, Lock
+import datetime
+import time
+
+from udp.server import Server
+from rps.server import Server as RpsSever
+
+PRUNE_TIME = 26
+
+class Lobby:
+    id: int
+    members: list[tuple[str, int]]
+    addr: int
+    gameServer: Server
+    severThread: Thread
+    gameId: int|None
+    heartbeat: bool|datetime.datetime
+    
+    def __init__(self, id:int, addr, gameId:int|None=None, rsaKey=None):
+        self.id = id
+        self.members = []
+        self.addr = addr
+        self.gameId = gameId
+        self.heartbeat = datetime.datetime.now()
+        match gameId:
+            case 1:
+                self.gameServer = RpsSever(addr, rsaKey=rsaKey, onClientJoin=self.onJoin, onClientLeave=self.onLeave, onReceiveData=self.receive)
+            case _:
+                self.gameServer = None
+        if self.gameServer != None:
+            self.severThread = Thread(target=self.gameServer.mainloop, daemon=True)
+        else:
+            raise ValueError(f"No such game with id={gameId}")
+            
+        
+    def start(self) -> None:
+        self.severThread.start()
+        
+    def stop(self) -> None:
+        self.gameServer.udpServer.isRunning.clear()
+        self.gameServer.isRunning = False
+                
+    def onJoin(self, addr):
+        self.members.append(addr)
+        self.heartbeat = True
+    
+    def onLeave(self, addr):
+        self.members.remove(addr)
+        if self.isEmpty():
+            self.heartbeat = datetime.datetime.now()
+    
+    def receive(self, addr, data):
+        pass
+    
+    def isNotFull(self) -> bool:
+        return self.gameServer.isNotFull()
+    
+    def isEmpty(self) -> bool:
+        # return self.gameServer.isEmpty()
+        return len(self.members) == 0
+    
+    def getMembers(self) -> list[int]:
+        return self.members
+    
+    def getGame(self) -> int|None:
+        return self.gameId
+    
+    def getAddr(self) -> tuple[str, int]:
+        return self.addr
+    
+    def isPrune(self):
+        if isinstance(self.heartbeat, datetime.datetime):
+            delta = self._heartbeatDelta()
+            if delta > PRUNE_TIME: # check if server has been empty for > PRUNE_TIME
+                return True
+        return False
+    
+    def _heartbeatDelta(self):
+        return (datetime.datetime.now() - self.heartbeat).seconds
+            
+    
+class LobbyHandler:
+    _autoIncrement: int
+    lobbies: list[Lobby]
+    lobbiesLock: Lock
+    pruneThread: Thread
+    isRunning: bool
+    rsaKey: None
+    
+    def __init__(self, rsaKey=None):
+        self.nextId = 1
+        self.lobbies = []
+        self.lobbiesLock = Lock()
+        self.pruneThread = Thread(target=self.prune, daemon=True)
+        self.isRunning = True
+        self.rsaKey = rsaKey
+        # 
+        self.startPrune()
+        
+    @property
+    def nextId(self):
+        id = self._autoIncrement
+        self._autoIncrement += 1
+        return id
+    
+    @nextId.setter
+    def nextId(self, v:int):
+        self._autoIncrement = v
+        
+    def stopPrune(self):
+        self.isRunning = False
+        
+    def startPrune(self):
+        self.pruneThread.start()
+    
+    def createLobby(self, addr, gameId:int|None=None) -> Lobby:
+        with self.lobbiesLock:
+            id = self.nextId
+            addr = (addr[0], addr[1]+id)
+            l = Lobby(id, addr, gameId=gameId, rsaKey=self.rsaKey)
+            l.start()
+            self.lobbies.append(l)
+            return l
+        
+    def deleteLobby(self, addr) -> bool|None:
+        with self.lobbiesLock:
+            lobbyIndex = [index for index, lobby in enumerate(self.lobbies) if lobby.addr == addr][:1]
+            lobbyIndex = lobbyIndex[0] if len(lobbyIndex) > 0 else None
+            if lobbyIndex != None:
+                self.lobbies[lobbyIndex].stop()
+                del self.lobbies[lobbyIndex]
+                return True
+            else:
+                raise ValueError(f"No such lobby addr {addr}")
+                return False
+        
+    def getAll(self) -> list[Lobby]:
+        with self.lobbiesLock:
+            return self.lobbies
+    
+    def getNotFull(self) -> list[Lobby]:
+        with self.lobbiesLock:
+            return [lobby for lobby in self.lobbies if lobby.isNotFull()]
+    
+    def getLobby(self, lobbyId:int) -> Lobby:
+        with self.lobbiesLock:
+            lobby = [lobby for lobby in self.lobbies if lobby.id == lobbyId][:1]
+            lobby = lobby[0] if len(lobby) > 0 else None
+            return lobby
+        
+    def findLobbies(self, gameId:int) -> Lobby:
+        with self.lobbiesLock:
+            lobbies = [lobby for lobby in self.lobbies if lobby.isNotFull() and lobby.gameId ==  gameId]
+            return lobbies
+        
+    def getMembers(self) -> dict[int, list[int]]:
+        with self.lobbiesLock:
+            return {lobby.id:lobby.members for lobby in self.lobbies}
+            
+    def prune(self):
+        while self.isRunning:
+            with self.lobbiesLock:
+                lobbies = self.lobbies.copy()
+            for lobby in lobbies:
+                # print(f"{lobby}, {lobby.isPrune()}, {lobby.heartbeat}")
+                if lobby.isPrune():
+                    print(f"DELETING LOBBY {lobby} due to PRUNE ({lobby._heartbeatDelta()})")
+                    self.deleteLobby(lobby.addr)
+            time.sleep(PRUNE_TIME)
+>>>>>>> Stashed changes
--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
+<<<<<<< Updated upstream
 from flask import Blueprint, redirect, render_template, request, url_for, abort, jsonify, g
 from flask_httpauth import HTTPBasicAuth
 from . import Statement
 
 main = Blueprint("main", __name__)
 auth = HTTPBasicAuth()
+=======
+<<<<<<< Updated upstream
+from flask import Blueprint, redirect, render_template, request, url_for
+from flask_login import login_required, current_user
+import json
+from . import db, Statement
+
+main = Blueprint("main", __name__)
+=======
+from flask import Blueprint, redirect, render_template, request, url_for, abort, jsonify, g
+from flask_httpauth import HTTPBasicAuth
+from . import Statement
+from .lobbies import LobbyHandler
+import udp.auth
+import base64
+
+main = Blueprint("main", __name__)
+auth = HTTPBasicAuth()
+rsaKey = udp.auth.generateRsaKey()
+lobbyHandler = LobbyHandler(rsaKey=rsaKey)
+>>>>>>> Stashed changes
 
 @auth.verify_password
 def verifyPassword(username, password):
@@ -14,6 +36,10 @@ def verifyPassword(username, password):
             return False
     g.account = account
     return True
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 # Index
 @main.route("/")
@@ -32,6 +58,7 @@ def createAccount():
     account = Statement.createAccount(username, password)
     return jsonify({"account-id":account.id, "username":account.username}), 201
 
+<<<<<<< Updated upstream
 @main.route("/auth/token")
 @auth.login_required
 def getAuthToken():
@@ -42,6 +69,22 @@ def getAuthToken():
 @auth.login_required
 def authTest():
     return jsonify({"hello":g.account.username})
+=======
+<<<<<<< Updated upstream
+# Errors
+@main.route("/error")
+def error():
+    # TODO: implement errors fully
+    errorCode = "404"
+    errorMessage = "Not Found: Requested file was not found"
+    return render_template("error.html", errorCode=errorCode, errorMessage=errorMessage, danger=True)
+
+<<<<<<< Updated upstream
+@main.route("/lobby/<int:lobbyId>")
+def match(lobbyId):
+    data = _JsonLobby(lobbyId)
+    return data
+>>>>>>> Stashed changes
 
 # game
 @main.route("/games/")
@@ -75,6 +118,7 @@ def createLobby():
     lobbyId = Statement.createLobby(game.id)
     return jsonify({"lobby-id":lobbyId}), 201
 
+<<<<<<< Updated upstream
 @main.route("/lobby/join", methods=["POST"])
 @auth.login_required
 def joinLobby():
@@ -82,3 +126,154 @@ def joinLobby():
     if not lobbyId:
         abort(400)
     return jsonify({"lobby-id":Statement.joinLobby(lobbyId, g.account.id)}), 201
+=======
+@main.route("/sandbox")
+def sandbox():
+    print("snad")
+    return "snad"
+
+def _JsonLobby(lobbyId):
+    return {"lobby":f"{Statement.getLobby(lobbyId)}", "members":f"{Statement.getMembers(lobbyId)}"}
+=======
+=======
+@main.route("/auth/token")
+@auth.login_required
+def getAuthToken():
+    return jsonify({"token":g.account.generateToken()})
+
+@main.route("/auth/key")
+@auth.login_required
+def getKey():
+    return jsonify({"key":base64.encodebytes(g.account.private_key).decode(),"account-id":g.account.id})
+
+@main.route("/auth/certificate")
+@auth.login_required
+def getCert():
+    # return server certificate
+    return None
+
+@main.route("/auth/certificate/validate")
+def validateCert():
+    valid = False
+    certificate = request.json.get("certificate")
+    certificate = base64.decodebytes(certificate.encode())
+    if certificate != None:
+        certificate = udp.auth.getCertificateFromDer(certificate)
+        attributes = udp.auth.getUserCertificateAttributes(certificate)
+        if attributes["account-id"] != None:
+            account = Statement.getAccount(attributes["account-id"])
+            publicKey = udp.auth.getRsaPublicFromDer(account.public_key)
+        else:
+            publicKey = rsaKey.public_key()
+        valid = udp.auth.validateCertificate(certificate, publicKey)
+        return jsonify({"valid":valid})
+    else:
+        abort(400) # missing args
+
+@main.route("/auth/test")
+@auth.login_required
+def authTest():
+    return jsonify({"hello":g.account.username})
+
+# game
+@main.route("/games/")
+@auth.login_required
+def getGames():
+    return jsonify({game.id:game.name for game in Statement.getGames()})
+
+@main.route("/lobby/all")
+@auth.login_required
+def getLobbies():
+    lobbies = LobbyHandler.getAll()
+    games = {game.id:game.name for game in Statement.getGames()}
+    data = lambda lobby: {"game":{"game-id":lobby.game_id, "game-name":games[lobby.game_id]},"size":Statement.getLobbySize(lobby.id),"is-full":Statement.getIsLobbyFree(lobby.id)}
+    return jsonify({lobby.id:data(lobby) for lobby in lobbies})
+
+@main.route("/lobby/create", methods=["POST"])
+@auth.login_required
+def createLobby():
+    gameId = request.json.get("game-id")
+    gameName = request.json.get("game-name")
+    if not (gameId or gameName): # check args
+        abort(400) # missing args
+    game = None
+    if gameId: # check gameId not null
+        game = Statement.getGame(gameId)
+    if not game: # check gameId null 
+        if gameName: # check gameName not null
+            game = Statement.findGame(gameName)
+    if not game: # check game null
+        abort(404) # no game found
+    addr = _getAddr()
+    lobby = lobbyHandler.createLobby(addr, game.id)
+    return jsonify({"lobby-id":lobby.id,"lobby-addr":lobby.getAddr(), "game-id":lobby.gameId}), 201
+
+def _getAddr():
+    host, port = request.host.split(":")
+    port = int(port)
+    return (host, port)
+    
+@main.route("/lobby/")
+@auth.login_required
+def getLobby():
+    lobbyId = request.json.get("lobby-id")
+    if not lobbyId:
+        abort(400) # missing args
+    lobby = lobbyHandler.getLobby(lobbyId)
+    return jsonify({"lobby-id":lobby.id,"lobby-addr":lobby.getAddr(), "game-id":lobby.gameId})
+
+@main.route("/lobby/members")
+@auth.login_required
+def getMembers():
+    return jsonify(lobbyHandler.getMembers)
+
+@main.route("/lobby/find")
+@auth.login_required
+def findLobby():
+    gameId = request.json.get("game-id")
+    gameName = request.json.get("game-name")
+    if not (gameId or gameName): # check args
+        abort(400) # missing args
+    game = None
+    if gameId: # check gameId not null
+        game = Statement.getGame(gameId)
+    if not game: # check gameId null 
+        if gameName: # check gameName not null
+            game = Statement.findGame(gameName)
+    if not game: # check game null
+        abort(404) # no game found
+    lobby = lobbyHandler.findLobbies(game.id)
+    lobby = lobby[0] if len(lobby) > 0 else None
+    if lobby != None:
+        return jsonify({"lobby-id":lobby.id,"lobby-addr":lobby.getAddr(), "game-id":lobby.gameId})
+    else:
+        abort(404)
+        
+@main.route("/friends/")
+@auth.login_required
+def getFriends():
+    friends = Statement.getFriends(g.account.id)
+    return jsonify({"friends":[{"id":account.id,"username":account.username} for account in friends]})
+    
+@main.route("/friends/add", methods=["POST"])
+@auth.login_required
+def addFriend():
+    username = request.json.get("username")
+    if username == None:
+        abort(400) # missing args
+    account = g.account
+    other = Statement.findAccount(username)
+    if other == None:
+        abort(404)
+    friends = Statement.createFriends(account.id, other.id)
+    return jsonify({"account":{"id":account.id,"username":account.username}, "other":{"id":other.id,"username":other.username}}), 201
+
+@main.route("/lobby/friends")
+@auth.login_required
+def getFriendLobbies():
+    friends = Statement.getFriends(g.account)
+    # lobbies = Statement.findLobby()
+    
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
