e8f6e367d3f86937326f12f0c5a427a3153b4f91
Harry Whitehorn <harrywhitehorn@outlook.com>
2024-05-06 17:35:56 +0100

    server refactor
    + ruff refactor

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,15 @@
-from flask import Flask
-import dotenv
 import os
 
-from udp import logger
+import dotenv
+from flask import Flask
+
+from udp import logger  # noqa: F401
 
 from .models import *
 
 dotenv.load_dotenv()
 PRUNE_TIME = int(os.environ.get("PRUNE_TIME"))
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,15 +17,16 @@ def create_app():
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
 
-    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY").encode()
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI") #'mysql://root:root@db:3306/paperclip'
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY").encode()
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     db.init_app(app)
-    
+
     with app.app_context():
         db.create_all()
 
     from .main import main as main_blueprint
+
     app.register_blueprint(main_blueprint)
 
     return app
